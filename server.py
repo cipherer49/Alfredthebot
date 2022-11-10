cf9b5043acf54583f5b092e11175b27862c25c76
cipherer49 <116225768+cipherer49@users.noreply.github.com>
2022-11-10 08:57:44 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,32 @@
+import socket
+
+# create a socket object
+s = socket.socket()
+print ("Socket successfully created")
+
+# reserve a port
+port = 12345
+
+# Next bind to the port
+s.bind(('', port))
+print ("socket binded to %s" %(port))
+
+# put the socket into listening mode
+s.listen(5)
+print ("socket is listening")
+
+# a forever loop until we interrupt it or
+# an error occurs
+while True:
+    # Establish connection with client.
+    c, addr = s.accept()
+    print('Got connection from', addr)
+
+    # send a thank you message to the client. encoding to send byte type.
+    c.send('Thank you for connecting'.encode())
+
+    # Close the connection with the client
+    c.close()
+
+    # Breaking once connection closed
+    break
